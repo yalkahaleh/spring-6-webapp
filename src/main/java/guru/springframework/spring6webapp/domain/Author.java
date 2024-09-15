183cc2ecb95ff599ac75a153db3f7b3d35d6529e
Yazan <y.alkahaleh@gmail.com>
2024-09-15 18:49:25 +0400

    initialize set

--- src/main/java/guru/springframework/spring6webapp/domain/Author.java
+++ src/main/java/guru/springframework/spring6webapp/domain/Author.java
@@ -3,6 +3,7 @@ package guru.springframework.spring6webapp.domain;
 
 import jakarta.persistence.*;
 
+import java.util.HashSet;
 import java.util.Set;
 
 @Entity
@@ -17,7 +18,7 @@ public class Author {
     private String lastName;
 
     @ManyToMany(mappedBy = "authors")
-    private Set<Book> books;
+    private Set<Book> books = new HashSet<Book>();
 
     public Set<Book> getBooks() {
         return books;
